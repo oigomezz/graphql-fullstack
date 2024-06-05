477c069bbdf4ca76daee58764c036494723ba51c
Oscar Ivan Gomez <oigomezz@unal.edu.co>
2024-06-05 12:06:10 -0500

    Added Prisma dependencies and updated Avocado model and resolver to use Prisma; updated schema.graphql and resolvers.ts accordingly.

--- api/src/resolvers.ts
+++ api/src/resolvers.ts
@@ -1,9 +1,13 @@
 import * as avo from './modules/avocado/avocado.resolver'
-
+import * as scalars from './modules/base/scalars.model'
 export default {
+  ...scalars,
   Query: {
     avo: avo.findOne,
     avos: avo.findAll,
+  },
+  Mutation: {
+    createAvo: avo.createAvo,
   },
   Avocado: avo.resolver,
 }
