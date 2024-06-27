13806bff48908ae38ffbbdb84ab67aa8828f9f2d
Oscar Ivan Gomez <oigomezz@unal.edu.co>
2024-06-27 13:10:44 -0500

    🚧 Replace useQuery with getStaticProps, fetch data at build time

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,16 +1,38 @@
-import { useQuery } from '@apollo/client'
+import type { GetStaticProps, InferGetStaticPropsType } from 'next'
 import Layout from '@components/Layout/Layout'
 import KawaiiHeader from '@components/KawaiiHeader/KawaiiHeader'
 import ProductList from '@components/ProductList/ProductList'
-import {
-  GetAllAvocadosDocument,
-  AvocadoFragment,
-} from '@service/graphql/graphql'
+import client from '@service/client'
+import { GetAllAvocadosDocument } from '@service/graphql/graphql'
+import type { AvocadoFragment } from '@service/graphql/graphql'
 
-const HomePage = () => {
-  const { data } = useQuery(GetAllAvocadosDocument)
+export const getStaticProps: GetStaticProps<{
+  products: AvocadoFragment[]
+}> = async () => {
+  try {
+    const response = await client.query({
+      query: GetAllAvocadosDocument,
+    })
 
-  let products = (data?.avos || []) as AvocadoFragment[]
+    if (response.data.avos == null) {
+      throw new Error(`There was an error fetching the items`)
+    }
+
+    const products = response.data.avos as AvocadoFragment[]
+    return { props: { products } }
+  } catch (e) {
+    console.log(e)
+    return {
+      props: {
+        products: [],
+      },
+    }
+  }
+}
+
+const HomePage = ({
+  products,
+}: InferGetStaticPropsType<typeof getStaticProps>) => {
   return (
     <Layout title="Home">
       <KawaiiHeader />
@@ -18,5 +40,4 @@ const HomePage = () => {
     </Layout>
   )
 }
-
 export default HomePage
